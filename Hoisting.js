34e0c693ad2371b85d2ca68b4b3ff5a3b3fa0cae
Dexterco <192400010@gift.edu.pk>
2023-12-30 17:48:30 +0500

    Hoisting Concept

---
+++ Hoisting.js
@@ -0,0 +1,7 @@
+//JavaScript Automatically shifts declaration of method and variables here at the top during compilation
+//Hoisting in JS
+//printing a variable 'a'
+console.log(b)
+//variable a is declared and ,
+// initialized after Printing
+var a = 10
